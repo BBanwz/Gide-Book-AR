dcbfbd7d73e13a605700729909ebc78ff4b80341
BBanwz <56763347+BBanwz@users.noreply.github.com>
2022-12-14 17:07:01 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'AR Gide book'
+copyright = '2022, CKTR'
+author = 'BBanwz'
 
 release = '0.1'
 version = '0.1.0'
